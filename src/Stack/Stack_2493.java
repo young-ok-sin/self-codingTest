bd7899c692c55b0c06a3a1cc9e6251a836e5173b
신영옥 <duddhrdl2727@naver.com>
2025-08-03 17:23:54 +0900

    day4 - fix

--- src/Stack/Stack_2493.java
+++ src/Stack/Stack_2493.java
@@ -1,26 +1,31 @@
 package Stack;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Stack_2493 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Stack<int[]> stack = new Stack<>();
-        int n = sc.nextInt();
-        int arr[] = new int[n];
+        int n = Integer.parseInt(br.readLine());
         int result[] = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         for(int i = 0; i < n; i++){
-            arr[i] = sc.nextInt();
+            int height = Integer.parseInt(st.nextToken());
             while(!stack.isEmpty()){
-                if(stack.peek()[0] >= arr[i]){
+                if(stack.peek()[0] >= height){
                     result[i] = stack.peek()[1] + 1;
                     break;
                 } else stack.pop();
             }
-
-            stack.push(new int[]{arr[i], i});
+            if(stack.isEmpty())
+                result[i] = 0;
+            stack.push(new int[]{height, i});
         }
 
         for(int i: result){
