5d66724a8ccb95bc76c796bbc7bdc68accc04d7a
ogi <87031572+young-ok-sin@users.noreply.github.com>
2025-09-03 23:25:41 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int dp[] = new int[n + 1];
+		dp[1] = 1;
+		if(2 <= n) dp[2] = 3;
+		
+		for(int i = 3; i <= n; i++) {
+			dp[i] = (dp[i-1] + (dp[i - 2] * 2) % 10007) % 10007;
+		}
+		System.out.println(dp[n]);
+	}
+
+}
