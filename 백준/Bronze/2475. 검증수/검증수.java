97b73cd455d588007d657b77ca9f7545d8aa2052
ogi <87031572+young-ok-sin@users.noreply.github.com>
2025-10-12 02:06:23 +0900

    [Bronze V] Title: 검증수, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+        int sum = 0;
+        for(int i = 0; i < 5; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        for(int i = 0; i < 5; i++){
+            arr[i] = arr[i] * arr[i];
+            sum += arr[i];
+        }
+        System.out.println(sum%10);
+    }
+}
