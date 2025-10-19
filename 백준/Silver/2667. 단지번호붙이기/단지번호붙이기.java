26f9595eff4fd6f5eb30b191101ce78e85f1f369
ogi <87031572+young-ok-sin@users.noreply.github.com>
2025-10-19 17:09:48 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,60 +1,66 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-	static int N, cnt;
-	static int[][] arr;
-	static int[] dx = {0,0,-1,1};
-	static int[] dy = {-1,1,0,0};
-	static boolean[][] visited;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		
-		arr = new int[N][N];
-		visited = new boolean[N][N];
-		
-		for(int i = 0; i < N; i++) {
-			String line = sc.next();
-			for(int j = 0; j < N; j++) {
-				arr[i][j] = line.charAt(j)- '0';
-			}
-		}
-		ArrayList<Integer> result = new ArrayList<>();
-		for(int i = 0; i < N; i++) {
-			for (int j = 0; j < N ; j++) {
-				if(visited[i][j] == false && arr[i][j] == 1) {
-					int size = dfs(i,j);
-					result.add(size);
-					cnt++;
-				}
-			}
-		}
-		Collections.sort(result);
-		System.out.println(cnt);
-		
-		for(int i : result) {
-			System.out.println(i);
-		}
-	}
-	
-	static int dfs(int x, int y) {
-		visited[x][y] = true;
-		int size = 1;
-		
-		for(int i = 0; i < 4; i++) {
-			int cx = x + dx[i];
-			int cy = y + dy[i];
-			if(cx >= 0 && cy >= 0 && cx < N && cy < N) {
-				if(arr[cx][cy] == 1 && !visited[cx][cy]) {
-					size += dfs(cx,cy);
-				}
-			}
-		}
-		return size;
-	}
+    static int N, arr[][], num_arr[], dx[] = {-1,1,0,0}, dy[] = {0,0,-1,1};
+    static boolean visited[][];
+
+    static class Point{
+        int x, y;
+        Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N][N];
+        for(int i = 0; i < N; i++){
+            String line = br.readLine();
+            for(int j =0; j < N; j++){
+                arr[i][j] = line.charAt(j) - '0';
+            }
+        }
+        visited = new boolean[N][N];
+        List<Integer> num_list = new ArrayList<>();
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(visited[i][j] ||  arr[i][j] == 0)   continue;
+                int cnt = bfs(i, j);
+                num_list.add(cnt);
+            }
+        }
+        System.out.println(num_list.size());
+        Collections.sort(num_list);
+        for(int a : num_list){
+            System.out.println(a);
+        }
+    }
+    static int bfs(int x, int y){
+        Queue<Point> q = new ArrayDeque<>();
+        visited[x][y] = true;
+        q.offer(new Point(x,y));
+        int cnt = 1;
+
+        while (!q.isEmpty()){
+            Point p = q.poll();
+            for(int d = 0; d < 4; d++){
+                int nx = p.x + dx[d];
+                int ny = p.y + dy[d];
+
+                if(nx < 0 || ny < 0 || nx >= N || ny >= N)  continue;
+                if(!visited[nx][ny] && arr[nx][ny] == 1){
+                    visited[nx][ny] = true;
+                    q.offer(new Point(nx,ny));
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
 }
