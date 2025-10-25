dfbb777e51c13fbfebe2afb75ba577134c86233c
ogi <87031572+young-ok-sin@users.noreply.github.com>
2025-10-25 23:44:52 +0900

    [Gold III] Title: 소문난 칠공주, Time: 640 ms, Memory: 268712 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,79 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class Main {
-    static char[][] a = new char[5][5];
-    static ArrayList<Pair> v = new ArrayList<>();
-    static int mask = 0;
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
-    static int ret = 0;
+    static char[][] arr;
+    static int[] dx = {-1,1,0,0}, dy = {0,0,-1,1};
+    static int cnt = 0;
+    static boolean[] selected = new boolean[25];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        arr = new char[5][5];
         for(int i = 0; i < 5; i++){
-            String s = sc.next();
+            String line = br.readLine();
             for(int j = 0; j < 5; j++){
-                a[i][j] = s.charAt(j);
-                v.add(new Pair(i, j));
+                arr[i][j] = line.charAt(j);
             }
         }
-        solve(0, 0);
-        System.out.println(ret);
+        combination(0,0);
+        System.out.println(cnt);
     }
-
-    private static void solve(int now, int cnt){
-        if(cnt == 7){
-            boolean[][] selected = new boolean[5][5];
-            int S = 0;
-            int Y = 0;
-            Pair p = null;
-            for(int i = 0; i < 25; i++){
-                if((mask & (1 << i)) != 0){
-                    p = v.get(i);
-                    selected[p.y][p.x] = true;
-                    if(a[p.y][p.x] == 'S') S++;
-                    else Y++;
-                }
-            }
-            if(Y > S) return;
-            Queue<Pair> q = new ArrayDeque<>();
-            q.offer(p);
-            selected[p.y][p.x] = false;
-            int count = 1;
-            while(!q.isEmpty()){
-                Pair tmp = q.poll();
-                int y = tmp.y;
-                int x = tmp.x;
-                for(int i = 0; i < 4; i++){
-                    int ny = y + dy[i];
-                    int nx = x + dx[i];
-                    if(ny < 0 || nx < 0 || ny >= 5 || nx >= 5) continue;
-                    if(selected[ny][nx]){
-                        count++;
-                        q.offer(new Pair(ny, nx));
-                        selected[ny][nx] = false;
-                    }
-                }
-            }
-            if(count == 7) ret++;
+    static void combination(int depth, int start){
+        if(depth == 7){
+            if(isValid()) cnt++;
             return;
         }
-        for(int i = now; i < 25; i++){
-            mask |= (1 << i);
-            solve(i + 1, cnt + 1);
-            mask &= ~(1 << i);
+        for(int i = start; i < 25; i++){
+            selected[i] = true;
+            combination(depth + 1, i + 1);
+            selected[i] = false;
         }
     }
-}
+    static boolean isValid(){
+        int s_cnt = 0;
+        List<int[]> list = new ArrayList<>();
 
-class Pair{
-    int y;
-    int x;
-    Pair(int y, int x){
-        this.y = y;
-        this.x = x;
+        for(int i = 0; i < 25; i++){
+            if(selected[i]){
+                int x = i / 5;
+                int y = i % 5;
+                list.add(new int[]{x,y});
+                if(arr[x][y] == 'S')    s_cnt++;
+            }
+        }
+        if(s_cnt < 4)   return false;
+        Queue<int[]> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[5][5];
+        q.add(list.get(0));
+        visited[list.get(0)[0]][list.get(0)[1]] = true;
+
+        int con_cnt = 1;
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+
+            for(int d = 0; d < 4; d++){
+                int nx = cur[0] + dx[d];
+                int ny = cur[1] + dy[d];
+                if(nx < 0 || ny < 0 || nx >= 5 || ny >= 5)  continue;
+
+                if(!visited[nx][ny] && selected[nx * 5 + ny]){
+                    visited[nx][ny] = true;
+                    q.add(new int[]{nx, ny});
+                    con_cnt++;
+                }
+            }
+        }
+        return con_cnt == 7;
     }
 }
