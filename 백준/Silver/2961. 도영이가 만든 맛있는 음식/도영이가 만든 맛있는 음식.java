309ae599de55cf26917541bf30e8ac96dace4145
ogi <87031572+young-ok-sin@users.noreply.github.com>
2025-10-26 23:13:18 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 96 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -5,33 +5,32 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int [] S,B;
-    static int minDiff = Integer.MAX_VALUE;
-
+    static int[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        S = new int[N];
-        B = new int[N];
-
+        arr = new int[N][2];
         for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            S[i] = Integer.parseInt(st.nextToken());
-            B[i] = Integer.parseInt(st.nextToken());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-        dfs(0,1,0,0);
-        System.out.println(minDiff);
-    }
-    static void dfs(int idx, int sour, int bitter, int cnt){
-        if(idx == N){
-            if(cnt > 0){
-                minDiff = Math.min(minDiff, Math.abs(sour - bitter));
-            }
-            return;
-        }
+        // 신맛은 곱, 쓴맛은 합
+        // 둘의 차이를 가장 작게
 
-        dfs(idx + 1, sour * S[idx], bitter + B[idx], cnt + 1);
-        dfs(idx + 1, sour, bitter, cnt);
+        int minDiff = Integer.MAX_VALUE;
+        for(int mask = 1; mask < (1 << N); mask++){
+            int sum = 0;
+            int multi = 1;
+            for(int i = 0; i < N; i++){
+                if((mask & (1 << i)) != 0){
+                    multi *= arr[i][0];
+                    sum += arr[i][1];
+                }
+            }
+            minDiff = Math.min(minDiff, Math.abs(multi - sum));
+        }
+        System.out.println(minDiff);
     }
 }
