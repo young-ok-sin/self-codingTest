7e2a1180654ada2f8be8acde3f3be96b9b9ce11b
ogi <87031572+young-ok-sin@users.noreply.github.com>
2025-11-27 22:17:21 +0900

    [Gold IV] Title: 키 순서, Time: 704 ms, Memory: 46912 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,67 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static int parents[];
-	static ArrayList<Integer>[] list, rev;
-	static boolean visited[];
-	 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		list = new ArrayList[N+1];
-		rev = new ArrayList[N+1];
-		for(int i = 1; i < N+1; i++) {
-			list[i] = new ArrayList<>();
-			rev[i] = new ArrayList<>();
-		}
-		
-		for(int i = 0; i < M; i++) {
-			StringTokenizer input = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(input.nextToken());
-			int b = Integer.parseInt(input.nextToken());
-			
-			list[a].add(b);
-			rev[b].add(a);
-		}
-		int ans = 0;
-		for(int i = 1; i <= N; i++) {
-			int big = bfs(i,list);
-			int small = bfs(i, rev);
-			
-			if(big + small == N-1) ans++;
-		}
-		System.out.println(ans);
-	}
-	static int bfs(int start, ArrayList<Integer>[] graph) {
-		boolean[] visited = new boolean[N+1];
-		Queue<Integer> q = new LinkedList<>();
-		q.add(start);
-		visited[start] = true;
-		int cnt = 0;
-		
-		while(!q.isEmpty()) {
-			int cur = q.poll();
-			for(int next : graph[cur]) {
-				if(!visited[next]) {
-					visited[next] = true;
-					q.add(next);
-					cnt++;
-				}
-			}
-		}
-		return cnt;
-		
-	}
+    static int N, M, cnt;
+    static ArrayList<Integer>[] graph, rgraph;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
+        graph = new ArrayList[N+1];
+        rgraph = new ArrayList[N+1];
+
+        for(int i = 1; i <= N; i++){
+            graph[i] = new ArrayList<>();
+            rgraph[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph[u].add(v);
+            rgraph[v].add(u);
+        }
+        for(int i = 1; i <= N; i++){
+            int u = bfs(i, graph);
+            int v = bfs(i, rgraph);
+            if(u+v == N-1)  cnt++;
+        }
+        System.out.println(cnt);
+
+    }
+    static int bfs(int start, ArrayList<Integer>[] list){
+        int result = 0;
+        boolean visited[] = new boolean[N+1];
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()){
+            int cur = q.poll();
+            for(int i : list[cur]){
+                if(!visited[i]){
+                    visited[i] = true;
+                    q.offer(i);
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
 }
