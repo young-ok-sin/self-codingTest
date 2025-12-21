35251f5a0b2a21ce14052d94bb190063df77f6c0
ogi <87031572+young-ok-sin@users.noreply.github.com>
2025-12-22 04:30:25 +0900

    [Gold IV] Title: 친구비, Time: 132 ms, Memory: 16916 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.java
+++ 백준/Gold/16562. 친구비/친구비.java
@@ -1,10 +1,10 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n, m, k;
     static int[] arr, parents;
-    static boolean[] visited;
     static void make(){
         for(int i = 1; i <= n; i++) parents[i] = i;
     }
@@ -30,7 +30,6 @@ public class Main {
 
         arr = new int[n+1];
         parents = new int[n+1];
-        visited = new boolean[n+1];
 
         st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++){
@@ -38,6 +37,7 @@ public class Main {
         }
 
         make();
+
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
             int v = Integer.parseInt(st.nextToken());
@@ -45,23 +45,17 @@ public class Main {
 
             union(v,w);
         }
-        int sum = 0;
+        int[] minCost = new int[n+1];
+        Arrays.fill(minCost, 10001);
 
         for(int i = 1; i <= n; i++){
             int root = find(i);
-            if(visited[root])  continue;
-            int min = Integer.MAX_VALUE;
-
-            for(int j = 1; j <= n; j++){
-                if(root == find(j)){
-                    min = Math.min(arr[j], min);
-                }
-            }
-            visited[root] = true;
-            sum += min;
+            minCost[root] = Math.min(minCost[root], arr[i]);
         }
-
-        if(sum <= k) System.out.println(sum);
-        else    System.out.println("Oh no");
+        int sum = 0;
+        for(int i =1; i <= n; i++){
+            if(parents[i] == i) sum += minCost[i];
+        }
+        System.out.println(sum <= k ? sum : "Oh no");;
     }
 }
