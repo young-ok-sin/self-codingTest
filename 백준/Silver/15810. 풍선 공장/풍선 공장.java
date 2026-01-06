6006f4a812767f23221d2b1f707ffacb267fa62b
ogi <87031572+young-ok-sin@users.noreply.github.com>
2026-01-07 01:44:10 +0900

    [Silver II] Title: 풍선 공장, Time: 704 ms, Memory: 121452 KB -BaekjoonHub

--- 백준/Silver/15810. 풍선 공장/풍선 공장.java
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.java
@@ -13,32 +13,29 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
 
         arr = new int[n];
-        st = new StringTokenizer(br.readLine());
-
         int max = 0;
-        for (int i = 0; i < n; i++) {
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
-            max = Math.max(max, arr[i]);
+            max = Math.max(arr[i], max);
         }
 
-        long start = 0;
+        long start = 1;
         long end = 1000000000000L;
-        long ret = end;
-
-        while (start <= end) {
+        long ret = 0;
+        while(start <= end){
             long mid = (start + end) / 2;
 
             long cnt = 0;
-            for (int i = 0; i < n; i++) {
+            for(int i = 0; i < n; i++){
                 cnt += mid / arr[i];
             }
 
-            if (cnt >= m) {
+            if(cnt >= m){
                 ret = mid;
                 end = mid - 1;
-            } else {
-                start = mid + 1;
             }
+            else start = mid + 1;
         }
         System.out.println(ret);
     }
