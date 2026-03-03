103cfcd84cea82aed169a76dab1e10973f9387fd
ogi <87031572+young-ok-sin@users.noreply.github.com>
2026-03-03 23:06:40 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 100 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n == 1) System.out.println(12 + " " + 1600);
+        if(n == 2) System.out.println(11 + " " + 894);
+        if(n == 3) System.out.println(11 + " " + 1327);
+        if(n == 4) System.out.println(10 + " " + 1311);
+        if(n == 5) System.out.println(9 + " " + 1004);
+        if(n == 6) System.out.println(9 + " " + 1178);
+        if(n == 7) System.out.println(9 + " " + 1357);
+        if(n == 8) System.out.println(8 + " " + 837);
+        if(n == 9) System.out.println(7 + " " + 1055);
+        if(n == 10) System.out.println(6 + " " + 556);
+        if(n == 11) System.out.println(6 + " " + 773);
+    }
+}
