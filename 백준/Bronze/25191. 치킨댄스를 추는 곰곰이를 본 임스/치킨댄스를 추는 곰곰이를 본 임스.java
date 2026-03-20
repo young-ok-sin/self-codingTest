0410dd75ccde6165d5b7485c7f168563dc9105ff
ogi <87031572+young-ok-sin@users.noreply.github.com>
2026-03-21 00:44:35 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스, Time: 68 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/25191. 치킨댄스를 추는 곰곰이를 본 임스/치킨댄스를 추는 곰곰이를 본 임스.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        a = a/2;
+        int sum = a + b;
+        if(n > sum){
+            System.out.println(sum);
+        } else if (n <= sum) {
+            System.out.println(n);
+        }
+    }
+}
