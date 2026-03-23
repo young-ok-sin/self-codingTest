695ebe9371fb34b0732873dc1fccd9a62af8bacb
ogi <87031572+young-ok-sin@users.noreply.github.com>
2026-03-23 22:22:16 +0900

    [Bronze IV] Title: Contemporary Art, Time: 68 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Bronze/16600. Contemporary Art/Contemporary Art.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        long a = Long.parseLong(br.readLine());
+        double ret = 4 * Math.sqrt(a);
+        System.out.println(ret);
+    }
+}
