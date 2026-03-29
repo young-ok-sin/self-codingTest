528e7e800bbddb598f0479cd753172f7d3606f3c
ogi <87031572+young-ok-sin@users.noreply.github.com>
2026-03-29 23:51:01 +0900

    [Bronze V] Title: Zadanie próbne, Time: 68 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println(reversed);
+    }
+}
