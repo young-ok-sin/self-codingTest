5c8ff0c1fe792d86a5e2b7468e53acd5eb60414f
ogi <87031572+young-ok-sin@users.noreply.github.com>
2026-04-20 23:51:47 +0900

    [Silver II] Title: Three Dots, Time: 1736 ms, Memory: 305332 KB -BaekjoonHub

--- 백준/Silver/13423. Three Dots/Three Dots.java
+++ 백준/Silver/13423. Three Dots/Three Dots.java
@@ -1,36 +1,12 @@
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    static int n,cnt;
-    static int[] arr;
-    static HashSet<Integer> set;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int test_case = Integer.parseInt(br.readLine());
-        for(int t = 1; t <= test_case; t++){
-            n = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            arr = new int[n];
-            set = new HashSet<>();
-            for(int i = 0; i < n; i++){
-                arr[i] = Integer.parseInt(st.nextToken());
-                set.add(arr[i]);
-            }
-            Arrays.sort(arr);
-            cnt = 0;
-            for(int i = 0; i < n; i++){
-                for(int j = i+1; j < n; j++){
-                    int ret = (arr[j] - arr[i]);
-                    if(set.contains(arr[j] + ret)){
-                        cnt++;
-                    }
-                }
-            }
-            sb.append(cnt).append("\n");
-        }
-        System.out.println(sb);
-    }
-}
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<center><h1>502 Bad Gateway</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
