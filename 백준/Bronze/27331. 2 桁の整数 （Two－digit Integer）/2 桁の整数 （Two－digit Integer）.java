492332fc9ac543effb8272eda48454d918b39029
ogi <87031572+young-ok-sin@users.noreply.github.com>
2026-04-23 02:01:57 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 64 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        System.out.print(n);
+        System.out.print(m);
+    }
+}
