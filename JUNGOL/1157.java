fd80541f6bc57cf892f9e98b513e9992efea1e79
ogi <87031572+young-ok-sin@users.noreply.github.com>
2026-06-16 00:51:42 +0900

    [Bronze 3] 버블정렬
    Implement bubble sort and print intermediate states.

---
+++ JUNGOL/1157.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class 1157 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < n - 1; i++){
+            for(int j = 0; j < n - 1 - i; j++){
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            for(int k = 0; k < n; k++){
+                System.out.print(arr[k] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
